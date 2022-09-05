e1620426b2abb2cb9ed126e047c95cb8b4d43c6a
thiago.tomaz <thiago.tomaz@zup.com.br>
2022-09-05 19:31:31 -0300

    metodo de filtrar pedidos por usuarios

--- src/main/java/br/com/zup/edu/delivery/pedido/pedido/controller/ListarPedidosPorCliente.java
+++ src/main/java/br/com/zup/edu/delivery/pedido/pedido/controller/ListarPedidosPorCliente.java
@@ -1,7 +1,6 @@
 package br.com.zup.edu.delivery.pedido.pedido.controller;
 
 import br.com.zup.edu.delivery.pedido.pedido.controller.response.PedidoPorClienteResponse;
-import br.com.zup.edu.delivery.pedido.pedido.model.Pedido;
 import br.com.zup.edu.delivery.pedido.pedido.model.StatusPedido;
 import br.com.zup.edu.delivery.pedido.pedido.repository.ClienteRepository;
 import br.com.zup.edu.delivery.pedido.pedido.repository.PedidoRepository;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -25,6 +23,7 @@ public class ListarPedidosPorCliente {
         this.clienteRepository = clienteRepository;
     }
 
+    // SELECT * FROM SV_COLETA WHERE EMISSAO BETWEEN '01.10.2016' AND '30.10.2016'
 
     @GetMapping("/cliente/{clienteId}")
     public ResponseEntity<?> pedidosPorCliente(@PathVariable Long clienteId,
@@ -33,15 +32,30 @@ public class ListarPedidosPorCliente {
                                                @RequestParam(required = false) StatusPedido status) {
 
 
-
-        if(desde == null && ate == null && status == null) {
+        if (status == null){
             List<PedidoPorClienteResponse> pedidos = pedidoRepository
-                    .findAllByClienteId(clienteId)
+                    .findAllByClienteId(
+                            clienteId
+                            )
                     .stream()
                     .map(PedidoPorClienteResponse::new).toList();
-            return ResponseEntity.ok(pedidos);
+
+              return ResponseEntity.ok(pedidos);
+
         }
 
+
+        List<PedidoPorClienteResponse> pedidos = pedidoRepository
+                .findAllByClienteIdAndStatus(
+                        clienteId,status
+                )
+                .stream()
+                .map(PedidoPorClienteResponse::new).toList();
+
         return ResponseEntity.ok(pedidos);
+
+
+
+
     }
 }
--- src/main/java/br/com/zup/edu/delivery/pedido/pedido/repository/PedidoRepository.java
+++ src/main/java/br/com/zup/edu/delivery/pedido/pedido/repository/PedidoRepository.java
@@ -1,10 +1,12 @@
 package br.com.zup.edu.delivery.pedido.pedido.repository;
 
-import br.com.zup.edu.delivery.pedido.pedido.model.Cliente;
 import br.com.zup.edu.delivery.pedido.pedido.model.Pedido;
+import br.com.zup.edu.delivery.pedido.pedido.model.StatusPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,5 +14,8 @@ public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
     public Optional<Pedido> findTopByClienteIdOrderByIdDesc(Long clientId);
 
-    List<Pedido> findAllByClienteId(Long id);
+  List<Pedido> findAllByClienteId(Long id);
+
+
+    List<Pedido> findAllByClienteIdAndStatus(Long clienteId, StatusPedido status);
 }
